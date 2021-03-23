3c26494370404aac672258c4be5b6afd24cdeaaa
Jordan Ziegler <jordanzeeg@gmail.com>
2021-03-23 16:52:52 -0700

    this is a test commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from function1 import add
 from function2 import sub
+from function3 import test
 
-choice = input("would you like to run function 1 or 2? ")
+choice = input("would you like to run function 1, 2, or 3? ")
 a = input("enter number 1: ")
 b = input("enter number 2: ")
 
@@ -10,5 +11,7 @@ a = int(a)
 b = int(b)
 if (choice == 1):
     print(add(a,b))
+elif choice == 3:
+    print(test(a,b))
 else:
     print(sub(a,b))
