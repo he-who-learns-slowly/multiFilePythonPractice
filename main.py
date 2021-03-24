047d0360cad0f7f936add39dc52ed96fbfef3948
Jordan Ziegler <jordanzeeg@gmail.com>
2021-03-23 18:27:07 -0700

    update changes for pull

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from function1 import add
 from function2 import sub
 from function3 import test
 from function4 import divide
-
+#this is a program
 choice = input("would you like to run function 1, 2, 3, or 4? ")
 a = input("enter number 1: ")
 b = input("enter number 2: ")
